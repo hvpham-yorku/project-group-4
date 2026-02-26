2c487cb4fea192f8ad372707ee886549972a0fea
Harneet <arrineet2005@gmail.com>
2026-02-26 15:06:58 -0500

    Initial Commit

---
+++ PersonalFinancialPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalFinancialPlanner
+//
+//  Created by Harneet Arri on 2026-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalFinancialPlanner/PersonalFinancialPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinancialPlannerApp.swift
+//  PersonalFinancialPlanner
+//
+//  Created by Harneet Arri on 2026-02-26.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinancialPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
